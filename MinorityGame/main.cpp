949afdf429153d9ea69732a00df76745fd505a7c
AngelStat <angelosdxdt@gmail.com>
2022-10-14 12:23:34 +0300

    fixed a bug

--- MinorityGame/main.cpp
+++ MinorityGame/main.cpp
@@ -30,17 +30,28 @@ void writeOutXY(std::string name, T iterable, T1 iterable1) {
 	file.close();
 }
 
+//template<typename T>
+//std::ostream& operator<<(std::ostream o, std::vector<T> v) {
+//	for (T x : v) o << x << ", ";
+//	return o;
+//}
+
+void printArray(uint *v, size_t sz) {
+	while (sz-- != 0) std::cout << *(v++) << ", ";
+	std::cout << std::endl;
+}
+
 
 
 void test() {
-	const uint N = 1001;
+	const uint N = 11;
 	auto booldist = std::uniform_int_distribution<uint>(0, 1);
 
 	auto players = std::vector<Player< (1 << 12)>> (N);
 	
-	uint memoryIterations = 11;
-	uint outerIterations = 10;
-	uint innerIterations = 1000;
+	const uint memoryIterations = 11;
+	const uint outerIterations = 10;
+	const uint innerIterations = 1000;
 
 	std::vector<double> sigma = std::vector<double>(memoryIterations);
 	std::vector<double> mean = std::vector<double>(memoryIterations);
@@ -49,7 +60,7 @@ void test() {
 	uint tally[2] = { 0 };
 	uint history = 2;
 
-	for (uint memory = 2; memory < 13; memory++) {
+	for (uint memory = 2; memory < 2+memoryIterations; memory++) {
 		//const uint memory = 2;
 		uint maxVal = (1 << memory);
 		PRINTLN(memory);
@@ -66,6 +77,13 @@ void test() {
 
 			}
 
+			//for (uint i = 0; i < N; i++) {
+			//	PRINTLN("P" << i);
+			//	printArray(players[i].strategy[0], 4);
+			//	printArray(players[i].strategy[1], 4);
+			//}
+
+
 
 			double runMean = 0;
 			double runSigma = 0;
@@ -73,14 +91,16 @@ void test() {
 
 
 			for (uint i = 0; i < innerIterations; i++) {
-
+				//PRINTLN("////");
 				for (auto p : players) {
 					uint bestStrategy = (p.performance[0] >= p.performance[1]) ? 0 : 1;
+					//PRINTLN(bestStrategy << " " << p.performance[0] << " " << p.performance[1]);
+					
 					tally[p.strategy[bestStrategy][history]]++;
 
 				}
 
-				uint minority = (tally[0] >= tally[1]) ? 1 : 0;
+				uint minority = (tally[0] >= tally[1]) ? 1U : 0U;
 
 				N1[i] = (double)tally[1];
 				runMean += N1[i]; //gotta be careful with overflow here
@@ -88,7 +108,7 @@ void test() {
 				//PRINTLN("tally was " << tally[0] << ", " << tally[1]);
 				//PRINTLN("minority was " << minority);
 
-				for (auto p : players) {
+				for (auto &p : players) {
 					p.performance[0] += (p.strategy[0][history] == minority);
 					p.performance[1] += (p.strategy[1][history] == minority);
 
